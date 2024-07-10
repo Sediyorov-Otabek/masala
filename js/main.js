e2c8de05d8276a49f86f6bd940a61ec8c1b58b88
Пользователь <otabeksediyorov298@gmail.com>
2024-07-10 07:05:42 +0500

    coe

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const main = document.querySelector(".main__box");
 const form = document.querySelector(".form");
 const main__up = document.querySelector(".main__up");
 const main__name = document.querySelector(".input");
+const body =document.querySelector("body")
 // console.log(input);
 
 main__name.addEventListener("click", () => {
@@ -12,6 +13,7 @@ main__up.addEventListener("click", () => {
 });
 function toggleShow() {
   main.classList.toggle("show");
+  
 }
 // {
 //     function solution(a, b) {
